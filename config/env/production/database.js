d6097f07c765acf72d3935194c4901bad563bc77
Mat Aldao <m@marcopolo.agency>
2025-10-28 22:49:39 -0300

    Pushing

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,17 +1,19 @@
-
 module.exports = ({ env }) => ({
     connection: {
       client: 'postgres',
       connection: {
-        host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT'),
-        database: env('DATABASE_NAME'),
-        user: env('DATABASE_USERNAME'),
-        password: env('DATABASE_PASSWORD'),
-        ssl: {
-          rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+        connectionString: env('DATABASE_URL'),
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        schema: env('DATABASE_SCHEMA', 'public'), // Not required
+        ssl:{
+            ca: env('DATABASE_CA', undefined),
         },
       },
+      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
       debug: false,
     },
   });
